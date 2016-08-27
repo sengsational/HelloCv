40879fc1ef277225da6f4771e3d02c47d6fb92d9
sengsational <buhtig.frog4food@recursor.net>
2016-08-27 11:39:34 -0400

    Changed to get native OpenCV to build

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,6 @@
 #include <jni.h>
 #include <string>
-//DRS 20160822d - Added include, passes compile check, fails external build
-#include <opencv2/core>
+#include <opencv2/core.hpp>
 
 extern "C" {
     jstring
@@ -15,9 +14,8 @@ extern "C" {
     //DRS 20160822c - Added native method
     jstring
     Java_com_company_cpp_hellocv_MainActivity_validate(JNIEnv *env, jobject, jlong addrGray, jlong addrRgba) {
-        //DRS 20160822d - Added OpenCv object (not successfully passing compile check)
-        //cv::Rect();
-        //cv::Mat();
+        cv::Rect();
+        cv::Mat();
         std::string hello2 = "Hello from validate";
         return env->NewStringUTF(hello2.c_str());
     }
